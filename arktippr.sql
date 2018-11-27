315684f585915cfa203fd421ef1bfadd83e7dc67
mschot <mschot@wownmedia.com>
2018-11-27 05:10:07 -0500

    V2 - initial updates

--- arktippr.sql
+++ arktippr.sql
@@ -3,7 +3,8 @@
 CREATE TABLE IF NOT EXISTS users (
   username varchar(30) PRIMARY KEY,
   address varchar(34) NOT NULL,
-  seed text NOT NULL
+  seed text NOT NULL,
+  platform varchar(8) SET DEFAULT 'reddit'
 );
 
 CREATE TABLE IF NOT EXISTS submissions (
